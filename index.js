d58190bbba2a5f7223b7c0eba8f66ab85fc363e8
Christopher Johnson <esreborn@gmail.com>
2019-01-30 14:39:10 -0500

    tweaking

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// const path = require('path')
+const path = require('path')
 
 module.exports = (options, ctx) => ({
 
-    // name: 'pdfjs',
-    //
-    // enhanceAppFiles: [
-    //     path.resolve(__dirname, 'enhanceAppFile.js')
-    // ],
+    name: 'pdfjs',
+
+    enhanceAppFiles: [
+        path.resolve(__dirname, 'enhanceAppFile.js')
+    ],
 
     ready () {
         console.log('vuepress-plugin-pdfjs ready!')
